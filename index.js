2abbe910a746b2d18272685f0d033fa9b544fabf
Mykhado <mikawestindies@gmail.com>
2022-09-22 10:33:38 +0200

    change but not click

--- index.js
+++ index.js
@@ -14,9 +14,39 @@ let conditionClim;
 let searchBtn = document.getElementById("btnSearch");
 let searchUser = document.getElementById("texte");
 
-searchBtn.addEventListener("click", () => {
+// searchBtn.addEventListener("click", () => {
+//   userChoice = searchUser.value;
+//   console.log(userChoice);
+//   fetch(`https://www.prevision-meteo.ch/services/json/${userChoice}`)
+//     .then((response) => response.json())
+//     .then((data) => {
+//       ville = data.city_info.name;
+//       pays = data.city_info.country;
+//       Tminday = data.fcst_day_0.tmin;
+//       Tmaxday = data.fcst_day_0.tmax;
+//       instantTemp = data.current_condition.tmp;
+//       date = data.current_condition.date;
+//       time = data.current_condition.hour;
+//       sunDown = data.city_info.sunset;
+//       sunUp = data.city_info.sunrise;
+//       imgMeteoBig = data.current_condition.icon_big;
+//       conditionClim = data.current_condition.condition;
+
+//       let imgTemps = document.getElementById("weatherPic");
+//       imgTemps.innerHTML = `<img src="${imgMeteoBig}" alt="">`;
+//       let condClim = document.getElementById("condition");
+//       condClim.innerHTML = `${ville}, ${pays} ${conditionClim}`;
+//       let today = document.getElementById("date-heure");
+//       today.innerHTML = `Le ${date} à ${time} `;
+//       let temperature = document.getElementById("tempMinMax");
+//       temperature.innerHTML = `min: ${Tminday}°  <span id="sizeTemp">${instantTemp}°</span>  max: ${Tmaxday}°`;
+//     });
+// });
+
+searchUser.addEventListener("change", () => {
   userChoice = searchUser.value;
   console.log(userChoice);
+  // alert("test");
   fetch(`https://www.prevision-meteo.ch/services/json/${userChoice}`)
     .then((response) => response.json())
     .then((data) => {
@@ -31,15 +61,14 @@ searchBtn.addEventListener("click", () => {
       sunUp = data.city_info.sunrise;
       imgMeteoBig = data.current_condition.icon_big;
       conditionClim = data.current_condition.condition;
-      
+
       let imgTemps = document.getElementById("weatherPic");
-      imgTemps.innerHTML=`<img src="${imgMeteoBig}" alt="">`
+      imgTemps.innerHTML = `<img src="${imgMeteoBig}" alt="">`;
       let condClim = document.getElementById("condition");
-      condClim.innerHTML= `${ville}, ${pays} ${conditionClim}`;
-      let today= document.getElementById("date-heure");
-      today.innerHTML= `Le ${date} à ${time} `
-      let temperature= document.getElementById("tempMinMax");
-      temperature.innerHTML=`min: ${Tminday}°  <span id="sizeTemp">${instantTemp}°</span>  max: ${Tmaxday}°`
- 
+      condClim.innerHTML = `${ville}, ${pays} ${conditionClim}`;
+      let today = document.getElementById("date-heure");
+      today.innerHTML = `Le ${date} à ${time} `;
+      let temperature = document.getElementById("tempMinMax");
+      temperature.innerHTML = `min: ${Tminday}°  <span id="sizeTemp">${instantTemp}°</span>  max: ${Tmaxday}°`;
     });
 });
